01fea7074efb888ad1e9fa6013ee950a4ed3f67d
Arjie Cristobal <ajie.io@yahoo.com>
2021-05-08 10:03:37 -0400

    5. Spring Boot Part 1

---
+++ microservicesBoot/src/main/java/ca/quickheaven/microservicesBoot/MicroservicesBootApplication.java
@@ -0,0 +1,13 @@
+package ca.quickheaven.microservicesBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBootApplication.class, args);
+	}
+
+}
